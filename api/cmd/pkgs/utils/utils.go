2a0460d68d3b975026081b8e990a491e852a4b71
Vojtech Kylar <90466277+KiskaLE@users.noreply.github.com>
2025-08-22 10:28:21 +0200

    fix: honor status code in WriteJSON

--- api/cmd/pkgs/utils/utils.go
+++ api/cmd/pkgs/utils/utils.go
@@ -8,7 +8,7 @@ import (
 
 func WriteJSON(w http.ResponseWriter, statusCode int, v any) error {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(statusCode)
 	return json.NewEncoder(w).Encode(v)
 }
 
