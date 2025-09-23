44bed6f25e10da22aa047604110f37c9ed9ffc6c
KiskaLE <vojtakylar@seznam.cz>
2025-09-23 15:10:43 +0200

    trpc init

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { env } from "./env";
+import { env } from "../env";
 import { createHash } from "crypto";
 
 export function cn(...inputs: ClassValue[]) {
