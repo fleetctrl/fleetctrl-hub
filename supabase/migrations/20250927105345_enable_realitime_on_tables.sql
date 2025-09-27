7358f96a87fd886949e990bdb05016e84e798b6e
KiskaLE <vojtakylar@seznam.cz>
2025-09-27 14:06:26 +0200

    feat: add migration backup script and update Docker configuration

---
+++ supabase/migrations/20250927105345_enable_realitime_on_tables.sql
@@ -0,0 +1,3 @@
+alter publication supabase_realtime add table public.computers;
+alter publication supabase_realtime add table public.tasks;
+alter publication supabase_realtime add table public.enrollment_tokens;
