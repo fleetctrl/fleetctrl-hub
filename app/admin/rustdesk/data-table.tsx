5017f7b6344da106f71b715793ec5ae5dd5d25d4
KiskaLE <vojtakylar@seznam.cz>
2025-10-06 12:02:20 +0200

    Removed unused TableCells

--- app/admin/rustdesk/data-table.tsx
+++ app/admin/rustdesk/data-table.tsx
@@ -150,7 +150,6 @@ function DataTable<TData, TValue>({
                       )}
                     </Fragment>
                   ))}
-                  <TableCell />
                 </TableRow>
               ))
             ) : (
