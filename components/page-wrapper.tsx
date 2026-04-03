3ccb5ed5b1c138986f0b59cf4774a1903fbc2444
KiskaLE <vojtakylar@seznam.cz>
2026-04-03 20:23:17 +0200

    fix: overflow issue

--- components/page-wrapper.tsx
+++ components/page-wrapper.tsx
@@ -8,10 +8,10 @@ type Props = {
 
 export default function PageWrapper({ children, siteHeader }: Props) {
   return (
-    <div>
+    <div className="min-w-0">
       <SiteHeader>{siteHeader}</SiteHeader>
-      <div className="flex flex-col items-center">
-        <div className="w-[1000px] md:max-w-[90%] flex flex-col justify-center items-center">
+      <div className="flex min-w-0 flex-col items-center px-4 md:px-0">
+        <div className="flex w-full max-w-[1000px] min-w-0 flex-col justify-center items-center md:w-[90%]">
           {children}
         </div>
       </div>
